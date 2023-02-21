8b8242949d14b164a578958c6b3fa1c69543ad0c
Nathan C Ilescas <nathan.ilescas@outlook.com>
2023-02-21 12:22:26 -0800

    added Basic_HTML_and_JavaScript_1/

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hello World");
+function myFunction() {
+		let string = "Kiss me, I'm Irish";
+		let result = string.fontcolor("green");
+		document.getElementById("Irish").innerHTML = 
+		result;
+		}
