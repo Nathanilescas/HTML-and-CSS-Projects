404ff456b5dce90e23b4b579ffd82a0bf222ae3b
Nathan C Ilescas <nathan.ilescas@outlook.com>
2023-03-10 20:31:12 -0800

    files

--- New_Practice_Folder/Hello_World.js
+++
@@ -1,8 +0,0 @@
-let greeting = "Hello World";
-console.log(greeting);
-
-let greeting2 = "Welcome to Node.js";
-console.log(greeting2);
-
-let sentence1 = "Learn how to debug code with the debugger";
-console.log(sentenceone);
